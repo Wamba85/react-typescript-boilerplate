4093e30ea6861d358f922652da9e24c78bbeba62
Andrea Mori <wamba85@gmail.com>
2020-04-15 13:17:31 +0200

    FIX minor bug config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
-module.exports =  {
-    semi:  false,
-    trailingComma:  'all',
-    singleQuote:  true,
-    printWidth:  120,
-    tabWidth:  2,
-  }
+module.exports = {
+  semi: false,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  endOfLine: 'auto',
+}
